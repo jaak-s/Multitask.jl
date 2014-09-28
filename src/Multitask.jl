a34f666fac077fe401351497452bb9485caf081f
Jaak Simm <jaak.simm@gmail.com>
2014-09-28 22:02:55 +0200

    sparse X support

--- src/Multitask.jl
+++ src/Multitask.jl
@@ -1,6 +1,7 @@
 module Multitask
 
 export nuclearNormMT
+export nuclearNormMTcol
 export power
 
 function power(A, n)
@@ -40,6 +41,43 @@ function nuclearNormMT(Xw,
         M = (1 - α) * M + α * tau * u * v'
     end
     return M
+end
+
+function nuclearNormMTs(Xw,
+                     Yw::Array{Array{Float64,1},1},
+                     tau,
+                     lambda=0.0, 
+                     nsteps=200)
+    Nfeat    = size(Xw[1], 1)
+    Ntasks   = length(Xw)
+    M = zeros(Nfeat, Ntasks)
+    D = zeros(M)
+    Xnz = map(x -> findnz(x), Xw)
+    for i = 0:(nsteps-1)
+        ## 1. negative gradient
+        for t = 1:Ntasks
+            #D[:,t] = - 2.0Xw[t] * (Xw[t]' * M[:,t] - Yw[t])
+            setSparseProd(D, t, Xnz[t][1], Xnz[t][2], Xnz[t][3], -2.0*(Xw[t]' * M[:,t] - Yw[t]) )
+            if lambda > 0
+              D[:,t] += - 2.0lambda * M[:,t]
+            end
+        end
+        ## 2. find 1st singular vectors
+        u, v, s = power(D, 10)
+        ## 3. add new solution
+        α = 2.0 / (i + 2)
+        M = (1 - α) * M + α * tau * u * v'
+    end
+    return M
+end
+
+function setSparseProd(res::Array{Float64,2}, col, xi, xj, xv, w)
+    for i = size(res, 1)
+        res[i, col] = 0.0
+    end
+    for i = size(xi, 1)
+        res[ xi[i], col ] += xv[i] * w[xj[i]]
+    end
 end
 
 end # module
